a6a447b0c296564d686f42a944f37aec20d48762
Alicia Frisone <a.frisone54@gmail.com>
2018-09-01 11:41:03 -0600

    Added training page and linked twitter feed

---
+++ public/footer.php
@@ -0,0 +1,128 @@
+        <!-- Start footer Area -->
+        <footer>
+            <div class="footer-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="about-company">
+                                <h3>About Company</h3>
+                                <p>Praesent vel rutrum purus. Nam vel dui eu risus duis dignissim dignissim. Suspen disse at eros tempus, congueconsequat.Fusce sit amet urna feugiat.Praesent vel rutrum purus. Nam vel dui eu risus.</p>
+                                <div class="social-icons">
+                                    <ul class="social-link">
+                                        <li class="first">
+                                            <a class="facebook" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li class="second">
+                                            <a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li class="third">
+                                            <a class="linkedin" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li class="fourth">
+                                            <a class="pint" href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li class="five">
+                                            <a class="skype" href="#"><i class="fa fa-skype" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li class="last">
+                                            <a class="youtube" href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="twitter-area">
+                                <h3>Twitter Feed</h3>
+                                <div class="twitter-list">
+                                    <ul>
+                                        <li>
+                                            <p><i class="fa fa-twitter" aria-hidden="true"></i>Looking for an awesome CREATIVE WordPress Theme? Esquise run even better.</p>
+                                            <a href="#">http://t.co/0WWEMQEQ48</a>
+                                            <p><span>3 Days ago</span></p>
+                                        </li>
+                                        <li>
+                                            <p><i class="fa fa-twitter" aria-hidden="true"></i>Looking for an awesome CREATIVE WordPress Theme? Esquise run even better.</p>
+                                            <a href="#">http://t.co/0WWEMQEQ48</a>
+                                            <p><span>3 Days ago</span></p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="flickr-photos">
+                                <h3>Flickr Photos</h3>
+                                <div class="flickr-list">
+                                    <ul>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr1.png" data-fancybox-group="flickr" title="flickr1"><img src="img/flickr/flickr1.png" alt="flickr1"></a>
+                                        </li>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr2.png" data-fancybox-group="flickr" title="flickr2"><img src="img/flickr/flickr2.png" alt="flickr2"></a>
+                                        </li>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr3.png" data-fancybox-group="flickr" title="flickr3"><img src="img/flickr/flickr3.png" alt="flickr3"></a>
+                                        </li>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr4.png" data-fancybox-group="flickr" title="flickr4"><img src="img/flickr/flickr4.png" alt="flickr4"></a>
+                                        </li>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr5.png" data-fancybox-group="flickr" title="flickr5"><img src="img/flickr/flickr5.png" alt="flickr5"></a>
+                                        </li>
+                                        <li>
+                                            <a class="fancybox" href="img/flickr/flickr6.png" data-fancybox-group="flickr" title="flickr6"><img src="img/flickr/flickr6.png" alt="flickr6"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="corporate-office">
+                                <h3>Corporate Office</h3>
+                                <div class="corporate-address">
+                                    <ul>
+                                        <li><i class="fa fa-send" aria-hidden="true"></i>44 New Design Street, rne 005</li>
+                                        <li><i class="fa fa-phone" aria-hidden="true"></i>(01) 800 433 633</li>
+                                        <li><i class="fa fa-envelope-o" aria-hidden="true"></i>info@gymedge.com</li>
+                                        <li><i class="fa fa-fax" aria-hidden="true"></i>Fax : (123) 4657890</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End footer Area -->
+            <!-- Start copyright area -->
+            <div class="copy-right-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-9 col-md-9 col-sm-9">
+                            <div class="copy-right">
+                                <p>© Copyrights gymedge 2016. All rights reserved. Designed by<a href="https://www.radiustheme.com/"> RadiusTheme</a></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3">
+                            <div class="visa-card">
+                                <ul>
+                                    <li>
+                                        <a href="#"><img src="img/visa-card.png" alt="visa-card"></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><img src="img/descover.png" alt="descover"></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><img src="img/paypal.png" alt="paypal"></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><img src="img/card.png" alt="card"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End copyright area -->
