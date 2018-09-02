9a05d2ec2074a529df30e50781c40132c9cdbb82
Alicia Frisone <alicia.frisone@workday.com>
2018-09-02 16:10:34 -0600

    removed email functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const email = require('./server/email')
+// const email = require('./server/email')
 
 const routerOptions = {
     root: path.join(__dirname, 'public')
